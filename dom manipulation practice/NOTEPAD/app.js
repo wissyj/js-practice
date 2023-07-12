f9c25072c28526cd117895385d04a3d2436be553
wisdomjohnson <wisdomjohnson207@gmail.com>
2023-07-12 17:55:48 +0100

    reload issue resolved

--- dom manipulation practice/NOTEPAD/app.js
+++ dom manipulation practice/NOTEPAD/app.js
@@ -4,7 +4,6 @@ const taskList = document.querySelector(".collection");
 const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
-const task_delete_el = document.createElement("button");
 
 // load all event listeners
 loadEventListeners();
@@ -14,9 +13,6 @@ function loadEventListeners() {
   form.addEventListener("submit", addTask);
   clearBtn.addEventListener("click", clearTasks);
   filter.addEventListener("keyup", filterTasks);
-  // update local storage value as user edits his input
-  taskList.addEventListener("click", updateTask);
-  task_delete_el.addEventListener("click", deleteTask);
 }
 // Get Tasks from LS
 function getTasks() {
@@ -28,48 +24,55 @@ function getTasks() {
   }
 
   notes.forEach(function (note) {
-    // Create li and svg element
+    // Create li element
     const li = document.createElement("li");
+    li.className = "collection-item cyan";
 
-    // Add class
-    li.className = "collection-item cyan ";
+    // Create textarea element
     const task_input_el = document.createElement("textarea");
     task_input_el.classList.add("text");
-    task_input_el.type = "text";
-    task_input_el.value = note;
     task_input_el.setAttribute("readonly", "readonly");
-    // Create text node and append to input
     task_input_el.appendChild(document.createTextNode(note));
-    // input in li tag
-    li.appendChild(task_input_el);
-    // edit button
+
+    // Create edit button
     const task_edit_el = document.createElement("button");
     task_edit_el.classList.add("edit");
     task_edit_el.innerText = "Edit";
-    // delete button
+
+    // Create delete button
+    const task_delete_el = document.createElement("button");
     task_delete_el.classList.add("delete");
     task_delete_el.innerText = "Delete";
-    // add any value entered in the input box as the text of the input_el tag
-    task_input_el.appendChild(document.createTextNode(taskInput.value));
-    // append elements to li tag
+
+    // Append elements to li tag
     li.appendChild(task_input_el);
     li.appendChild(task_edit_el);
     li.appendChild(task_delete_el);
-    // edit item
-    task_edit_el.addEventListener("click", (e) => {
-      if (task_edit_el.innerText.toLowerCase() == "edit") {
+
+    // Append li to ul
+    taskList.appendChild(li);
+
+    // Edit button event listener
+    task_edit_el.addEventListener("click", function (e) {
+      if (task_edit_el.innerText.toLowerCase() === "edit") {
         task_edit_el.innerText = "Save";
         task_input_el.removeAttribute("readonly");
         task_input_el.focus();
       } else {
         task_edit_el.innerText = "Edit";
         task_input_el.setAttribute("readonly", "readonly");
+        const newContent = task_input_el.value;
+        const taskIndex = Array.from(taskList.children).indexOf(li);
+        updateTaskInLocalStorage(taskIndex, newContent);
       }
+      e.preventDefault();
     });
-    // delete item
-    task_delete_el.addEventListener("click", deleteTask);
-    // Append li to ul
-    taskList.appendChild(li);
+    // Delete button event listener
+    task_delete_el.addEventListener("click", function (e) {
+      removeTaskFromLocalStorage(li);
+      taskList.removeChild(li);
+      e.preventDefault();
+    });
   });
 }
 // add task event
@@ -86,7 +89,6 @@ function addTask(e) {
   task_input_el.type = "text";
   task_input_el.value = taskInput.value;
   task_input_el.setAttribute("readonly", "readonly");
-
   // edit button
   const task_edit_el = document.createElement("button");
   task_edit_el.classList.add("edit");
@@ -106,24 +108,37 @@ function addTask(e) {
 
   // append li  tag to ul tag
   taskList.appendChild(listItem);
+
   // STORE IN lS
   storeTaskInLocalStorage(taskInput.value);
+
   // clear input box
   taskInput.value = "";
   e.preventDefault();
   // edit el
-  task_edit_el.addEventListener("click", (e) => {
-    if (task_edit_el.innerText.toLowerCase() == "edit") {
-      task_edit_el.innerText = "Save";
+  task_edit_el.addEventListener("click", function (e) {
+    if (this.innerText.toLowerCase() == "edit") {
+      this.innerText = "Save";
       task_input_el.removeAttribute("readonly");
       task_input_el.focus();
     } else {
-      task_edit_el.innerText = "Edit";
+      this.innerText = "Edit";
       task_input_el.setAttribute("readonly", "readonly");
+      const listItem = this.parentElement;
+      const newContent = task_input_el.value;
+      const taskIndex = Array.from(taskList.children).indexOf(listItem);
+      updateTaskInLocalStorage(taskIndex, newContent);
     }
+    e.preventDefault();
+  });
+
+  // Delete button event listener
+  task_delete_el.addEventListener("click", function (e) {
+    removeTaskFromLocalStorage(li);
+    taskList.removeChild(li);
+    e.preventDefault();
   });
 }
-task_delete_el.addEventListener("click", deleteTask);
 
 // store task in LS
 function storeTaskInLocalStorage(note) {
@@ -137,16 +152,7 @@ function storeTaskInLocalStorage(note) {
 
   localStorage.setItem("notes", JSON.stringify(notes));
 }
-// update task event
-function updateTask(e) {
-  if (e.target.classList.contains("edit")) {
-    const listItem = e.target.parentElement;
-    const task_input_el = listItem.querySelector("textarea.text");
-    const newContent = task_input_el.value;
-    const taskIndex = Array.from(taskList.children).indexOf(listItem);
-    updateTaskInLocalStorage(taskIndex, newContent);
-  }
-}
+
 // update task in LS
 function updateTaskInLocalStorage(taskIndex, newContent) {
   let notes;
@@ -160,14 +166,7 @@ function updateTaskInLocalStorage(taskIndex, newContent) {
 
   localStorage.setItem("notes", JSON.stringify(notes));
 }
-// delete task event
-function deleteTask(e) {
-  if (e.target.classList.contains("delete")) {
-    const li = e.target.parentElement;
-    removeTaskFromLocalStorage(li);
-    taskList.removeChild(li);
-  }
-}
+
 // Remove from LS
 function removeTaskFromLocalStorage(taskItem) {
   let notes;
@@ -189,9 +188,9 @@ function removeTaskFromLocalStorage(taskItem) {
 function clearTasks(e) {
   // you can also use - taskList.innerHTML ='' but the one used below has been proved to be faster;
   while (taskList.firstChild) {
-    if (confirm("Are you sure you want to delete all permanently?")) {
-      taskList.removeChild(taskList.firstChild);
-    }
+    // if (confirm("Are you sure you want to delete all permanently?")) {
+    taskList.removeChild(taskList.firstChild);
+    // }
   }
   // Clear from LS
   clearTasksFromLocalStorage();
@@ -208,7 +207,7 @@ function filterTasks(e) {
   document.querySelectorAll(".collection-item").forEach(function (task) {
     const item = task.firstChild.textContent;
     if (item.toLowerCase().indexOf(text) != -1) {
-      task.style.display = "block";
+      task.style.display = "flex";
     } else {
       task.style.display = "none";
     }
@@ -231,8 +230,6 @@ plusSign.addEventListener("click", function (e) {
       '<svg id="plusSign" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" /></svg>';
   }
 });
-// edit text in li after creation
-
 // // practice
 // const shot = document.querySelector(".collection");
 // shot.onmouseover = function () {
